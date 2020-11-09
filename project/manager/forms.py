bffb985fd55cff059e789965e1b03c7e299d3aa6
Patryk <w.patryk@o2.pl>
2020-11-09 21:40:04 +0100

    remove register

--- project/manager/forms.py
+++ project/manager/forms.py
@@ -3,9 +3,3 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User  # import modeli z bazdy danych wbudowanej w django
 from django import forms
 
-
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
-
--- project/manager/views.py
+++ project/manager/views.py
@@ -1,42 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
-
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login, logout
-from .forms import CreateUserForm
 
-
-def register_page(request):
-    form = CreateUserForm()
-
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Utworzono konto dla:' + user)
-
-            return redirect('login')
-
-    context = {'form': form}
-    return render(request, 'register.html', context)
+from django.contrib.auth.decorators import login_required
 
 
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('searching_report')
-        else:
-            messages.info(request, "Username OR password is incorrect")
-    return render(request, 'login.html')
+            if user is not None:
+                login(request, user)
+                return redirect('searching_report')
+            else:
+                messages.info(request, "Username OR password is incorrect")
+        return render(request, 'login.html')
 
 
 def logout_user(request):
@@ -44,6 +29,7 @@ def logout_user(request):
     return redirect('login')
 
 
+@login_required(login_url='login')
 def homepage(request):
     return render(request, 'home.html')
 
@@ -52,14 +38,17 @@ def password_change(request):
     return render(request, 'password_change.html')
 
 
+@login_required(login_url='login')
 def searching_client(request):
     return render(request, 'services/searching_client.html')
 
 
+@login_required(login_url='login')
 def searching_report(request):
     return render(request, 'services/searching_report.html')
 
 
+@login_required(login_url='login')
 def adding_report(request):
     return render(request, 'services/adding_report.html')
 
